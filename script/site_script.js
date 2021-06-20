95466462fffa7c180074bb5e5b1f499ce5a60ebb
world-art <77616557+world-art@users.noreply.github.com>
2021-06-20 16:42:26 +0300

    new script

---
+++ script/site_script.js
@@ -0,0 +1,14 @@
+
+var styles = [
+	'background: red',
+	'background: orange',
+	'background: gold',
+	'background: green',
+	'background: blue',
+	'background: steelblue',
+	'background: darkviolet'
+	];
+	
+	console.log ( '%c А %c Н %c Д %c Р %c Е %c Й ',
+			styles[0], styles[1], styles[2], styles[3],
+			styles[4], styles[5]);
